18ff883db4d34bd09584d28ca35c44a2387c5113
PeterOeClausen <PeterOeClausen@gmail.com>
2017-02-14 13:21:08 +0100

    I just added systemtime to GazeDataLog

--- src/JuliusSweetland.OptiKey/Services/CSVLogService.cs
+++ src/JuliusSweetland.OptiKey/Services/CSVLogService.cs
@@ -91,7 +91,10 @@ namespace JuliusSweetland.OptiKey.Services
         {
             if (doLogGazeData)
             {
-                //data data
+                //Getting system datetime:
+                DateTime systemTimeStamp = DateTime.Now;
+
+                //data data:
                 string dataTimeStamp = data.TimeStampString;
                 bool dataIsFixated = data.IsFixated;
                 int dataState = data.State;
@@ -119,8 +122,8 @@ namespace JuliusSweetland.OptiKey.Services
                 double rightSmoothedCoordinateY = data.RightEye.SmoothedCoordinates.Y;
 
                 //Creating new line:
-                var newLine = string.Format("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11},{12},{13},{14},{15},{16},{17},{18},{19},{20},\n",
-                    dataTimeStamp, dataIsFixated, dataState, dataSmoothedCoordinateX, dataSmoothedCoordinateY, dataRawCoordiateX, dataRawCoordiateY,
+                var newLine = string.Format("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11},{12},{13},{14},{15},{16},{17},{18},{19},{20},{21}\n",
+                    systemTimeStamp, dataTimeStamp, dataIsFixated, dataState, dataSmoothedCoordinateX, dataSmoothedCoordinateY, dataRawCoordiateX, dataRawCoordiateY,
                     leftPupilCenterCoordinateX, leftPupilCenterCoordinateY, leftPupilSize, leftRawCoordinateX, leftRawCoordinateY, leftSmoothedCoordinateX, leftSmoothedCoordinateY,
                     rightPupilCenterCoordinateX, rightPupilCenterCoordinateY, rightPupilSize, rightRawCoordinateX, rightRawCoordinateY, rightSmoothedCoordinateX, rightSmoothedCoordinateY);
 
