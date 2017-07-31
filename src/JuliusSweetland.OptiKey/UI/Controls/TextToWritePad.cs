5893e170fd21c6990b1891c5853c5ca6a9e48a52
Peter Øvergård Clausen <PeterOeClausen@gmail.com>
2017-07-31 18:44:43 +0200

    Added TextToWritePad on top of ScratchPad. I now need to wire it up in the backend. "Next sentence button" and so on.

---
+++ src/JuliusSweetland.OptiKey/UI/Controls/TextToWritePad.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace JuliusSweetland.OptiKey.UI.Controls
+{
+    public class TextToWritePad : UserControl
+    {
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("TextToWritePadText", typeof (string), typeof (TextToWritePad), new PropertyMetadata(default(string)));
+
+        public string TextToWritePadText
+        {
+            get { return (string) GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+    }
+}
