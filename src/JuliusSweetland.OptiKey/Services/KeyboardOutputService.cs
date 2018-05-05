4eaa03d011e8a6ce185886c3981d12125659c440
tbafna <tanyabafna22@gmail.com>
2018-05-05 17:00:46 +0200

    backone changed to not delete the complete word but only the last letter/character Also the danish phrases created

--- src/JuliusSweetland.OptiKey/Services/KeyboardOutputService.cs
+++ src/JuliusSweetland.OptiKey/Services/KeyboardOutputService.cs
@@ -132,9 +132,13 @@ namespace JuliusSweetland.OptiKey.Services
                     break;
 
                 case FunctionKeys.BackOne:
+                    // var backOneCount = string.IsNullOrEmpty(lastTextChange)
+                    //    ? 1 //Default to removing one character if no lastTextChange
+                    //    : lastTextChange.Length;
+
                     var backOneCount = string.IsNullOrEmpty(lastTextChange)
-                        ? 1 //Default to removing one character if no lastTextChange
-                        : lastTextChange.Length;
+                       ? 1 //Default to removing one character if no lastTextChange
+                       : 1;
 
                     var textChangedByBackOne = false;
 
