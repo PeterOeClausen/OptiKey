100dc5fda5a10cabdf0dddceac3b6dd89221b883
tbafna <tanyabafna22@gmail.com>
2019-01-29 10:36:23 +0100

    decreasing dwell time from 1s is again possible

--- src/JuliusSweetland.OptiKey/UI/ViewModels/MainViewModel.ServiceEventHandlers.cs
+++ src/JuliusSweetland.OptiKey/UI/ViewModels/MainViewModel.ServiceEventHandlers.cs
@@ -1109,7 +1109,7 @@ namespace JuliusSweetland.OptiKey.UI.ViewModels
 
                 case FunctionKeys.DecreaseDwellTime:
                     Log.Info("Decreasing DwellTime.");
-                    if (Settings.Default.KeySelectionTriggerFixationDefaultCompleteTime.Milliseconds > 0)
+                    if (Settings.Default.KeySelectionTriggerFixationDefaultCompleteTime.Milliseconds > 0 || Settings.Default.KeySelectionTriggerFixationDefaultCompleteTime.Seconds > 0)
                         Settings.Default.KeySelectionTriggerFixationDefaultCompleteTime -= TimeSpan.FromMilliseconds(100); //Decrease Dwelltime //+ an int to Timespan converter?
                     break;
 
